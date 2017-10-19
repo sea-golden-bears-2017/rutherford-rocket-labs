b2c9350ad636c99f729c6f83013d6c94aad408db
Anna Jeszeck <email": null>
2017-10-19 16:02:11 -0700

    Add form to process order page, add functionality to mark quantities received.

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -7,4 +7,17 @@ class OrdersController < ApplicationController
     @order = Order.find(params[:id])
     @orderpart = OrdersPart.new()
   end
+
+  def update
+    p params
+    @order = Order.find(params[:id])
+    params[:part_id].each do |key, value|
+      orderpart = OrdersPart.find(key)
+      orderpart.quantity_received = value
+      orderpart.save
+    end
+    @order.submitted = true
+    @order.processed = true
+    render :show
+  end
 end
--- app/controllers/orders_parts_controller.rb
+++ app/controllers/orders_parts_controller.rb
@@ -1,8 +1,3 @@
 class OrdersPartsController < ApplicationController
-  def update
-    @orderpart = OrdersPart.find(params[:id])
-    @orderpart.quantity_received = params[:quantity_received]
-    @orderpart.save
-    render :'orders/show'
-  end
+  
 end
