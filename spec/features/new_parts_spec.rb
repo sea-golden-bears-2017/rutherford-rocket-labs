d7a2d103bb5116d6227a5fec404c0d9b9a4e57fa
Gordon Baker <email": null>
2017-10-19 12:03:00 -0700

    WIP: feature tests giving false positives

--- spec/features/new_parts_spec.rb
+++ spec/features/new_parts_spec.rb
@@ -12,4 +12,15 @@ RSpec.feature "NewParts", type: :feature do
     expect(page.has_content?('Save Part'))
 
   end
+
+  scenario "Adds renders errors if part not valid" do
+    visit "/"
+    click_link "New Part"
+    fill_in 'Min', with: 10
+    fill_in 'Max', with: 200
+
+    click_button('Save Part')
+    expect(page.has_content?("Name can't be blank"))
+
+  end
 end
