70a32eaac34beb7459de794ebafbec3a16bc9e09
Anna Jeszeck <email": null>
2017-10-18 17:18:08 -0700

    Add seedfile and faker.

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,5 @@
+require 'faker'
+
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
 #
@@ -5,3 +7,26 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Part.destroy_all
+Order.destroy_all
+OrdersPart.destroy_all
+
+
+parts = []
+orders = []
+
+10.times do
+  part = Part.create(name: Faker::Science.element)
+  parts << part
+end
+
+
+10.times do
+  order = Order.create()
+  orders << order
+end
+
+
+5.times do
+  OrdersPart.create(order: orders.sample, part: parts.sample, quantity_ordered: 4)
+end
