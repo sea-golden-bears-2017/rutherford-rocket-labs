bce5305d47affaac2c0dd26ae2dd4c13a91779ff
Gordon Baker <email": null>
2017-10-19 10:48:54 -0700

    Implement saving new part object and redirect

--- app/controllers/parts_controller.rb
+++ app/controllers/parts_controller.rb
@@ -5,4 +5,19 @@ class PartsController < ApplicationController
 
   def new
   end
+
+  def create
+    @part = Part.new(part_params)
+    if @part.save
+      redirect_to parts_path
+    else
+      render new_part_path
+    end
+
+  end
+
+private
+  def part_params
+      params.require(:part).permit(:name, :min, :max)
+  end
 end
