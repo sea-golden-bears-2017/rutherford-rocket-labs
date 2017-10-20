8ac0d90a5c8dd4c5dc799e3dfbfc7a82b42ad620
Anna Jeszeck <email": null>
2017-10-20 11:01:41 -0700

    Add optional: true to belongs_to declaration in user model, tests pass.

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,8 +1,8 @@
 class Order < ApplicationRecord
   has_many :orders_parts
   has_many :parts, through: :orders_parts
-  belongs_to :creator, class_name: :User
-  belongs_to :processor, class_name: :User
+  belongs_to :creator, class_name: :User, optional: true
+  belongs_to :processor, class_name: :User, optional: true
 
   def houston_time(type)
     if type.to_s == 'created'
