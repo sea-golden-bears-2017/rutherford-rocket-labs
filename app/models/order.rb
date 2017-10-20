a9ebc0b1568ca20fa35602a87a3525ae22f521c7
Gordon Baker <email": null>
2017-10-19 18:46:48 -0700

    Add houston_time method to process into houston time

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,4 +1,10 @@
 class Order < ApplicationRecord
   has_many :orders_parts
   has_many :parts, through: :orders_parts
+
+  def houston_time
+    time = self.created_at.in_time_zone("Central Time (US & Canada)")
+    time.strftime("%Y-%m-%d %H:%M:%S") + " Houston Time"
+  end
+
 end
