5df4499e6359cafa1297026ba8487038729ee251
Gordon B <gordon.indeed@gmail.com>
2017-10-22 16:32:39 -0700

    Adds error handling for part submission

--- app/controllers/orders_parts_controller.rb
+++ app/controllers/orders_parts_controller.rb
@@ -5,11 +5,29 @@ class OrdersPartsController < ApplicationController
   end
 
   def create
+    @errors = []
     order = Order.find(params[:order_id])
-    part = Part.find(params[:orders_part][:part_id])
-    quantity = params[:orders_part][:quantity].to_i
-    @orders_part = OrdersPart.create(order: order, part: part, quantity_ordered: quantity)
-    redirect_to order_path(order)
+    part_id = params[:orders_part][:part_id]
+    quantity = params[:orders_part][:quantity]
+
+    if part_id == ""
+      @errors << "Must enter a part number"
+    end
+    if quantity == ""
+      @errors << "Must enter a quantity"
+    end
+    if !Part.exists?(part_id)
+      @errors << "Could not find part with id #{part_id}"
+    end
+
+    if @errors.empty?
+      part = Part.find(part_id)
+      quantity = quantity.to_i
+      @orders_part = OrdersPart.create(order: order, part: part, quantity_ordered: quantity)
+      redirect_to order_path(order)
+    else
+      render 'new', locals: { '@order': order }
+    end
   end
 
 end
