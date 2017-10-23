eabdec3545d4077ee7405b36296540f4a9f61e61
Ashishjayandar <email": null>
2017-10-22 17:54:49 -0700

    the tests for the parts controller and the orders controller completed

--- spec/controllers/orders_controller_spec.rb
+++ spec/controllers/orders_controller_spec.rb
@@ -53,5 +53,43 @@ RSpec.describe OrdersController, type: :controller do
     end
     let!(:warehouse) { Warehouse.create(location: "Seattle") }
     let!(:order1) { Order.create(warehouse: warehouse, creator: user1, processor: user2) }
+
+    it "after creating a new entry returns a status 302" do
+      session[:user_id] = user1.id
+      get :create, params: {order: {warehouse: "#{warehouse.id}"}}
+      expect(response.status).to eq 302
+    end
   end
+  describe "orders#update" do
+    let!(:user1) do
+      user1 = User.new(first_name: "Anna", last_name: "J", username: "annaj")
+      user1.password = 'password'
+      user1.save
+      user1
+    end
+    let!(:user2) do
+      user2 = User.new(first_name: "Ash", last_name: "J", username: "ashj")
+      user2.password = 'password'
+      user2.save
+      user2
+    end
+    let!(:warehouse) { Warehouse.create(location: "Seattle") }
+    let!(:order1) { Order.create(warehouse: warehouse, creator: user1, processor: user2) }
+    let!(:part1) { Part.create(name: "Test Part") }
+    let!(:orderspart) { OrdersPart.create(order: order1, part: part1, quantity_ordered: 2) }
+    it "updates the submitted order " do
+      session[:user_id] = user1.id
+      get :update, params: {id: order1.id, order: {submitted: true}}
+      expect((assigns[:order]).submitted).to eq true
+    end
+    it "updates an order which is being processed" do
+      session[:user_id] = user1.id
+      get :update, params: {id: order1.id, part_id: {orderspart.id => 2}}
+      expect((assigns[:order]).processed).to eq true
+    end
+  end
+
+
+
+
 end
